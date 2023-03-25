04eab77725d182df2abd3a90b4481796051599cb
e-id <open-eid@outlook.com>
2023-03-25 01:38:44 +0100

    Linux preferences

--- src/util/preferences.ts
+++ src/util/preferences.ts
@@ -1,4 +1,7 @@
 import * as os from 'os'
+import * as fs from 'fs'
+import * as path from 'path'
+
 import { execSync } from 'child_process'
 
 export class Preferences {
@@ -23,6 +26,17 @@ export class Preferences {
       }
     }
 
+    if (os.platform() === 'linux') {
+      try {
+        const storeFile = path.join(os.homedir(), '.' + this.nameSpace + '.json')
+        const preferences = JSON.parse(fs.readFileSync(storeFile).toString())
+        return preferences[key]
+      } catch (e) {
+        this.lastError = e.message
+        return null
+      }
+    }
+
     return null
   }
 
@@ -31,6 +45,17 @@ export class Preferences {
     if (os.platform() === 'darwin') {
       try {
         execSync(`defaults write ${this.nameSpace} ${key} -string "${value}"`)
+      } catch (e) {
+        this.lastError = e.message
+      }
+    }
+
+    if (os.platform() === 'linux') {
+      try {
+        const storeFile = path.join(os.homedir(), '.' + this.nameSpace + '.json')
+        const preferences = JSON.parse(fs.readFileSync(storeFile).toString())
+        preferences[key] = value
+        fs.writeFileSync(storeFile, JSON.stringify(preferences))
       } catch (e) {
         this.lastError = e.message
       }
