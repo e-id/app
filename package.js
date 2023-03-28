3d7346ebbefb0c23d93c312a4766946940e13df6
e-id <open-eid@outlook.com>
2023-03-28 17:48:12 +0200

    Package

--- package.js
+++ package.js
@@ -1,20 +1,25 @@
 const fs = require('fs')
 const os = require('os')
+const path = require('path')
 
 const { exec } = require('pkg')
 const { execSync } = require('node:child_process')
 
 exec(['--compress', 'Brotli', '.']).then(() => {
   if (os.platform() === 'darwin') {
-    execSync('osacompile -o dist/e-id.app src/mac/launcher.applescript')
-    execSync('plutil -replace LSUIElement -bool true dist/e-id.app/Contents/Info.plist')
-    execSync('plutil -replace CFBundleIdentifier -string io.github.e-id dist/e-id.app/Contents/Info.plist')
-    execSync('plutil -replace CFBundleURLTypes -array dist/e-id.app/Contents/Info.plist')
-    execSync('plutil -insert CFBundleURLTypes -json \'{ "CFBundleURLName" : "e-ID", "CFBundleURLSchemes" : [ "e-id", "open-eid" ] }\' -append dist/e-id.app/Contents/Info.plist')
-    fs.writeFileSync('dist/e-id.app/Contents/Resources/applet.icns', fs.readFileSync('assets/icon.icns'))
-    if (fs.existsSync('dist/e-id.app/Contents/MacOS/e-id')) {
-      fs.unlinkSync('dist/e-id.app/Contents/MacOS/e-id')
+    console.log(execSync('osacompile -o "dist/Open e-ID.app" src/mac/launcher.applescript').toString())
+    console.log(execSync('plutil -replace LSUIElement -bool true "dist/Open e-ID.app/Contents/Info.plist"').toString())
+    console.log(execSync('plutil -replace CFBundleIdentifier -string io.github.e-id "dist/Open e-ID.app/Contents/Info.plist"').toString())
+    console.log(execSync('plutil -replace CFBundleURLTypes -array "dist/Open e-ID.app/Contents/Info.plist"').toString())
+    console.log(execSync('plutil -insert CFBundleURLTypes -json \'{ "CFBundleURLName" : "e-ID", "CFBundleURLSchemes" : [ "e-id", "open-eid" ] }\' -append "dist/Open e-ID.app/Contents/Info.plist"').toString())
+    fs.writeFileSync('dist/Open e-ID.app/Contents/Resources/applet.icns', fs.readFileSync('assets/icon.icns'))
+    if (fs.existsSync('dist/Open e-ID.app/Contents/MacOS/e-id')) {
+      fs.unlinkSync('dist/Open e-ID.app/Contents/MacOS/e-id')
     }
-    fs.copyFileSync('dist/e-id', 'dist/e-id.app/Contents/MacOS/e-id')
+    fs.copyFileSync('dist/e-id', 'dist/Open e-ID.app/Contents/MacOS/e-id')
+    if (fs.existsSync('dist/Open e-ID.app.zip')) {
+      fs.unlinkSync('dist/Open e-ID.app.zip')
+    }
+    console.log(execSync('zip -vr "Open e-ID.app.zip" "Open e-ID.app/" -x "*.DS_Store"', { cwd: path.join(__dirname, '/dist') }).toString())
   }
 })
